9926ad5e0c7b2e06001be4f93935c3842c96c268
Vineet Sridhar <vineets1600@gmail.com>
2020-10-21 20:37:45 -0400

    Part one completed (Sorry I forgot we had to use git until the end)

---
+++ app/src/main/java/com/example/flixter/Models/Movie.java
@@ -0,0 +1,46 @@
+package com.example.flixter.Models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+
+    public static List<Movie> fromJsonArray(JSONArray array) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < array.length(); i++){
+            movies.add(new Movie(array.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
